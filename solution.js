1c0ce5f845a576be428a2719deb0d5c935b9f5b5
xstartstaticx <daniel.reiller@icloud.com>
2022-07-05 12:51:57 +0200

    finished exercise

---
+++ solution.js
@@ -0,0 +1,13 @@
+// 1
+let introSentence = "hello, my name is Fran and I am";
+let age = 25;
+
+console.log(introSentence + " " + age);
+// since introSentence is a string, the + operator converts everything to a string. Age was a number, and is now a string.
+
+// 2
+let integer = "1005";
+console.log(parseInt(integer));
+
+let float = "10.05";
+console.log(parseFloat(float));
